9d299d37ce570b65de5afd9fbdff37bcf944bcdb
Gabriel Jose <gabriel.soares@arphoenix.com.br>
2021-03-24 16:24:37 -0300

    Codigo documentado

--- iris_python.py
+++ iris_python.py
@@ -7,33 +7,33 @@ Created on Wed Mar 24 14:53:09 2021
 
 import pandas as pd
 
-df = pd.read_csv("iris.data")
+df = pd.read_csv("iris.data") # Importando o nosso dataser
 df.rename(
     columns={
         '5.1': 'sepal length (cm)', 
         '3.5': 'sepal width (cm)', 
         '1.4': 'petal length (cm)', 
         '0.2': 'petal width (cm)' 
-        }, inplace = True)
-df.info()
-df.describe(include="all")
-x = df.drop("Iris-setosa", axis=1)
-y = df["Iris-setosa"]
+        }, inplace = True)  # Renomeando as colunas
+df.info()  # Verificando as informaçoes
+df.describe(include="all")  # Verificando a descrição completa do dataset
+x = df.drop("Iris-setosa", axis=1) # Separando a variavel preditora (x = df.iloc[:, :-1].values)
+y = df["Iris-setosa"]  # Separando a variavel targe (y = df.iloc[:, 4].values)
 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler # Importando o modulo para fazer a normalização dos dados
 scaler = StandardScaler()
 scaler = scaler.fit(x)
-x = scaler.transform(x)
+x = scaler.transform(x)  # Transformando os dados
 
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20)
+from sklearn.model_selection import train_test_split  # Importando o modulo de treino
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20)  # Treino do modelo
 
 from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors = 5)  #O 5 é o padrao
+classifier = KNeighborsClassifier(n_neighbors = 5)  # O 5 é o padrao
 classifier.fit(x_train, y_train) 
 y_pred = classifier.predict(x_test)
 
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix # Avaliando o Algoritmo 
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred)) 
 
