e9143359a9b2871b228329e8d6e8a9e1b90b29b6
Gabriel Jose <gabriel.soares@arphoenix.com.br>
2021-03-24 16:13:35 -0300

    Modelo de machine-learning pronto, só falta fazer a explicação do processo

--- iris_python.py
+++ iris_python.py
@@ -15,3 +15,25 @@ df.rename(
         '1.4': 'petal length (cm)', 
         '0.2': 'petal width (cm)' 
         }, inplace = True)
+df.info()
+df.describe(include="all")
+x = df.drop("Iris-setosa", axis=1)
+y = df["Iris-setosa"]
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler = scaler.fit(x)
+x = scaler.transform(x)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5)  #O 5 é o padrao
+classifier.fit(x_train, y_train) 
+y_pred = classifier.predict(x_test)
+
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred)) 
+
